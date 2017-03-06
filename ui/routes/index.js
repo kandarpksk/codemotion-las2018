6c07ba0f609257da4a916f5ec2197ab69b932b62
Kandarp Khandwala <kkhandwala@ucsd.edu>
2017-03-05 17:24:40 -0800

    defined video metadata

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -8,9 +8,9 @@ function readSubtitle(filename) {
 	process.stdout.write('loading subtitle...')
 
 	const fs = require('fs')
-	var i, contents = fs.readFileSync('public/'+filename, 'utf8').split('\n')
+	var contents = fs.readFileSync('public/'+filename, 'utf8').split('\n')
 	subtitles = {}
-	for(i = 0; i < contents.length; i++) {
+	for(var i = 0; i < contents.length; i++) {
 		var j = contents[i].indexOf(' ')
 		var t = contents[i].substr(0, j).split(':')
 		subtitles[parseInt(t[0])*60 + parseInt(t[1])] = contents[i].substr(j+1)
@@ -42,17 +42,29 @@ function addSegment(start, text, l) {
 	})
 }
 
-readSubtitle('other/video3.txt')
 var data = {
-	name: 'CS50 2016 - Week 8 - Python',
-	url: 'videos/video3.mp4',
+	name: 'tutorial title',
+	url: 'video_path',
 	segments: []
 }
-addSegment(0, '//show accumulated code', 'js')
-addSegment(410, '#todo', 'py')
-addSegment(3600, '', 'c')
 
 exports.view = function(req, res) {
+	var vnum = (req.query.vnum) ? req.query.vnum : 3
+	var metadata = { name: 'Sample Video',
+			start: [0, 410, 3600],
+			code: ['//show accumulated code', '#todo', ''],
+			l: ['js', 'py', 'c']
+		} // dummy
+	if (req.query.vnum != undefined)
+		metadata = require('../public/other/video'+vnum+'.json')
+	
+	readSubtitle('other/video'+vnum+'.txt')
+	data.name = metadata.name
+	data.url = 'videos/video'+vnum+'.mp4'
+	data.segments = []
+	for (var i = 0; i < metadata.start.length; i++)
+		addSegment(metadata.start[i], metadata.code[i], metadata.l[i])
+
 	res.render('index', data)
 }
 
