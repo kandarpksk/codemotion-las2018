feb5177850e6b9ba4decbd45646afe8c2cd09686
Kandarp Khandwala <kkhandwala@ucsd.edu>
2017-03-18 13:47:56 -0700

    checkpoint: diff

--- v1/backend/codem.py
+++ v1/backend/codem.py
@@ -17,22 +17,27 @@ while success:
 	t_sec = int(math.floor((fnum/fps)%60)) #check
 
 	if fnum%fps == 1:
-		if numpy.array_equal(image, prev):
+		diff = cv2.subtract(image, prev)
+		imgray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+		ndiff = cv2.countNonZero(imgray)
+		if ndiff < 1:
 			if not ignoring: print
-			print '\rignoring %d:%02d' % (t_min, t_sec),
 			ignoring = True
+			print '\rignoring %d:%02d' % (t_min, t_sec),
 		else:
+			if ignoring: print
 			ignoring = False
-			diff = cv2.subtract(prev, image)
 			marked = image.copy()
-			imgray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 			ret, thresh = cv2.threshold(imgray, 60, 255, 0)
 			contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 			cv2.drawContours(marked, contours, -1, (0,255,0), 1)
 
-			print '\rprocessing %d:%02d now' % (t_min, t_sec),
+			print '\rprocessing %d:%02d now (%d differences)' % (t_min, t_sec, ndiff),
 			sys.stdout.flush()
-			cv2.imwrite('../public/extracts/video'+str(vnum)+'/frame'+str(fnum)+'.jpg', marked)
+			path = '../public/extracts/video'+str(vnum)
+			name = str(fnum)+('ignore' if ndiff < 7500 else 'frame')+'.jpg'
+			cv2.imwrite(path+'/'+name, marked)
 
 			prev = image
 			# end of else stmt
