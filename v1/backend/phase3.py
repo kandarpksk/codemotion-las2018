0567a70db743f623811178dc4b399656c79ed120
Kandarp Khandwala <kkhandwala@ucsd.edu>
2017-03-25 13:35:47 -0700

    staged

--- v1/backend/phase3.py
+++ v1/backend/phase3.py
@@ -6,7 +6,32 @@ print 'starting with frame', fnum, '\n'
 
 path = '../public/extracts/video'+str(vnum)
 
-code = ['']
+def compare(known, txt):
+	d = dmp.diff_match_patch()
+	diffs = d.diff_main(known, txt, False)
+	d.diff_cleanupSemantic(diffs)
+	l, total, change = [[]], 0, 0
+	for x in diffs:
+		lines = re.split('\n|\\n', x[1])
+		for part in lines:
+			total += len(part)
+			if len(part)>1:
+				if x[0] == 1:
+					if l[-1] and l[-1][-1] == 'mod':
+						l[-1].pop() # big changes?
+					else: l[-1].append('new')
+					change += len(part)
+				elif x[0] == -1:
+					l[-1].append('mod')
+					change += len(part)
+				else:
+					l[-1].append('sim')
+
+				if part != lines[-1]:
+					l.append([]) #print list(set(l[-2]))
+	return int(round(change*100./total)), d, diffs
+
+buffer = ['']
 read, th, inc = True, 0, 0
 while fnum < 216000:
 	s = 3 # read number of segments
@@ -15,7 +40,7 @@ while fnum < 216000:
 		#if not read: print
 		sys.stdout.write("\r100%\033[K")
 		# previous count
-		print '\r%d: frame %d' % (len(code)-1, fnum),
+		print '\r%d: frame %d' % (len(buffer)-1, fnum),
 		sys.stdout.flush()
 		read = True
 	except IOError:
@@ -24,10 +49,10 @@ while fnum < 216000:
 			th += 5000
 		sys.stdout.write("\r100%\033[K")
 		# previous count
-		print '\r%d: frame %d missing' % (len(code)-1, fnum),
+		print '\r%d: frame %d missing' % (len(buffer)-1, fnum),
 		sys.stdout.flush()
 		read = False
-		#s = 0
+		#s = 0 #maybe
 	file.close()
 
 	# read text from each segment
@@ -48,54 +73,23 @@ while fnum < 216000:
 				tag = 'unlikely'
 
 		if txt != '' and tag != 'unlikely':
-			if txt == code[-1] or txt in code:
+			if txt == buffer[-1] or txt in buffer:
 				f = open(path+'/%s/frame%d-segment%di.html' % (tag, fnum, snum), 'w')
 				# todo: move related files
 				f.write('<pre>' + txt.replace('\n', '<br/>') + '</pre>')
 				f.close()
 			else:
-				d = dmp.diff_match_patch()
-				diffs = d.diff_main(code[-1], txt, False)
-				d.diff_cleanupSemantic(diffs)
-				l, total, change = [[]], 0, 0
-				for x in diffs:
-					lines = re.split('\n|\\n', x[1])
-					for part in lines:
-						total += len(part)
-						if len(part)>1:
-							# print len(l),
+				pc, d, diffs = compare(buffer[-1], txt)
 
-							if x[0] == 1:
-								# print '+', part.rstrip(), '\t',
-								if l[-1] and l[-1][-1] == 'mod':
-									l[-1].pop() # big changes?
-								else: l[-1].append('new')
-								change += len(part)
-							elif x[0] == -1:
-								# print '-', part.rstrip(), '\t',
-								l[-1].append('mod')
-								change += len(part)
-							else:
-								l[-1].append('sim')
-								# print '~',
-							## print part.rstrip(), '\t',
-
-							if part != lines[-1]:
-								l.append([])
-								# print list(set(l[-2]))
-					## print
-				pc = int(round(change*100./total))
-				# print str(pc)+'%',
-
-				# if len(code) > 4:
+				# if len(buffer) > 4:
 					# todo: print '\nreached buffer capacity'
-					# code.pop(0)
+					# buffer.pop(0)
 				if pc < 70:
 					inc += 1
-					code[-1] = txt
+					buffer[-1] = txt
 				else:
 					# print '\n', txt
-					code.append(txt)
+					buffer.append(txt)
 
 				#move related files too
 				f = open(path+'/%s/frame%d-segment%d.html' % (tag, fnum, snum), 'w')
